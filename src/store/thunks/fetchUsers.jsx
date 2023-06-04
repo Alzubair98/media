638dd8da0c13b37d8e254f52561852e2fbcaa1cd
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-06-04 21:52:48 +0300

    add one second pause in the app

--- src/store/thunks/fetchUsers.jsx
+++ src/store/thunks/fetchUsers.jsx
@@ -4,7 +4,17 @@ import axios from "axios";
 const fetchUsers = createAsyncThunk("users/fetch/pending", async () => {
   const response = await axios.get("http://localhost:3005/users");
 
+  //dev only
+  await pause(1000);
+
   return response.data;
 });
+
+// FOR DEV ONLY
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
