acf0a5af44360d866bab1fb989a85a061e28bf25
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-06-05 18:01:17 +0300

    add style to skeletion loader

--- src/cmponents/Skeleton.jsx
+++ src/cmponents/Skeleton.jsx
@@ -1,10 +1,32 @@
 import classNames from "classnames";
 
 const Skeletion = ({ times }) => {
+  const outerCassNames = classNames(
+    "relative",
+    "overflow-hidden",
+    "bg-gray-200",
+    "mb-2.5",
+    "rounded"
+  );
+  const innerClassNames = classNames(
+    "animate-shimmer",
+    "absolute",
+    "inset-0",
+    "-translate-x-full",
+    "bg-gradient-to-r",
+    "from-gray-200",
+    "via-white",
+    "to-gray-200"
+  );
+
   const boxes = Array(times)
     .fill(0)
     .map((_, i) => {
-      return <div key={i} />;
+      return (
+        <div key={i} className={outerCassNames}>
+          <div className={innerClassNames} />
+        </div>
+      );
     });
 
   return boxes;
