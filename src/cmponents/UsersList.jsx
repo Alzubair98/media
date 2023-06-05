31ff04271854ca2b67afe6c1d64fd2e8fbd7dec0
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-06-05 18:05:50 +0300

    finish the loader

--- src/cmponents/UsersList.jsx
+++ src/cmponents/UsersList.jsx
@@ -15,7 +15,7 @@ const UsersList = () => {
   }, [dispatch]);
 
   if (isLoading) {
-    return <Skeletion times={6} className="h-10 w-full" />;
+    return <Skeletion times={6} className="h-20 w-full mt-4" />;
   }
 
   if (error) {
