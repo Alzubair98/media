3c149684508a3fa547ebb60a892ad29da21944c3
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-06-12 14:23:12 +0300

    update expandable Panel

--- src/cmponents/ExpandablePanel.jsx
+++ src/cmponents/ExpandablePanel.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+import { GoChevronDown, GoChevronUp } from "react-icons/go";
+
 const ExpandablePanel = ({ header, children }) => {
+  const [expanded, setExpanded] = useState(false);
   return (
     <div className="mb-2 border rounded">
       <div className="flex p-2 justify-between items-center cursor-pointer">
         <div className="flex flex-row items-center justify-between">
           {header}
         </div>
+        <div onClick={() => setExpanded(!expanded)}>
+          {expanded ? <GoChevronUp /> : <GoChevronDown />}
+        </div>
       </div>
-      <div className="p-2 border-t">{children}</div>
+      {expanded && <div className="p-2 border-t">{children}</div>}
     </div>
   );
 };
