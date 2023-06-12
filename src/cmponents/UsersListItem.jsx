a2748860ecde9b945030a7d5e91edebfcdc3d880
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-06-12 14:15:53 +0300

    add expandable panel & edit user list

--- src/cmponents/UsersListItem.jsx
+++ src/cmponents/UsersListItem.jsx
@@ -2,6 +2,7 @@ import { GoTrashcan } from "react-icons/go";
 import Button from "./Button";
 import { deleteUser } from "../store";
 import { useThunk } from "../hooks/use-thunk";
+import ExpandablePanel from "./ExpandablePanel";
 
 const UsersListItem = ({ user }) => {
   const [doRemoveUser, isDeletingUser, error] = useThunk(deleteUser);
@@ -9,23 +10,17 @@ const UsersListItem = ({ user }) => {
   const handleClick = () => {
     doRemoveUser(user);
   };
-  return (
-    <div className="mb-2 border rounded">
-      <div className="flex p-2 justify-between items-center cursor-pointer">
-        <div className="flex flex-row items-center justify-between">
-          <Button
-            className="mr-4"
-            loading={isDeletingUser}
-            onClick={handleClick}
-          >
-            <GoTrashcan />
-          </Button>
-          {error && <div>Error deleting user.</div>}
-          {user.name}
-        </div>
-      </div>
-    </div>
+
+  const header = (
+    <>
+      <Button className="mr-4" loading={isDeletingUser} onClick={handleClick}>
+        <GoTrashcan />
+      </Button>
+      {error && <div>Error deleting user.</div>}
+      {user.name}
+    </>
   );
+  return <ExpandablePanel header={header}>Content!!!</ExpandablePanel>;
 };
 
 export default UsersListItem;
