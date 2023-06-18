d51aa2086c27904542410aeff235a142c5e71233
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-06-18 23:20:14 +0300

    update the reducer inside the index js

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { albumsApi } from "./apis/albumsApi";
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
   },
 });
 
