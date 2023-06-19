61c6bf62083a5afac36e7fd5bdc1ad9d2e8cc6dc
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-06-19 17:48:35 +0300

    show the albums

--- src/cmponents/AlbumList.jsx
+++ src/cmponents/AlbumList.jsx
@@ -1,10 +1,33 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skeletion from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 const Albumslist = ({ user }) => {
   const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-  console.log(data);
-  return <div> Albums for {user.name}</div>;
+  let content;
+  if (isLoading) {
+    content = <Skeletion times={3} />;
+  } else if (error) {
+    content = <div> Error loading albums.</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          list of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+
+  return (
+    <div>
+      <div>abums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 };
 
 export default Albumslist;
