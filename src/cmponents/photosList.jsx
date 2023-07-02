498d4252a025b64c1651ecac924a81bb6f497117
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-07-02 22:29:29 +0300

    add and actıvate add photos button

--- src/cmponents/photosList.jsx
+++ src/cmponents/photosList.jsx
@@ -1,10 +1,25 @@
 import PhotosListItems from "./photosListItem";
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, useAddPhotoMutation } from "../store";
+import Button from "./Button";
 
 const PhotosList = ({ album }) => {
   useFetchPhotosQuery(album);
+  const [addPhoto, results] = useAddPhotoMutation();
 
-  return "photo list";
+  const handleAddPhoto = () => {
+    addPhoto(album);
+  };
+
+  return (
+    <>
+      <div className="m-2 flex flex-row justify-between items-center">
+        <h3 className="text-lg font-bold">photo In {album.title}</h3>
+        <Button loading={results.isLoading} onClick={handleAddPhoto}>
+          + Add Photo
+        </Button>
+      </div>
+    </>
+  );
 };
 
 export default PhotosList;
